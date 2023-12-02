1b3c65f93ea15c493f487a1622eff59c09446009
memespraysneks <calebsjseeman@gmail.com>
2023-12-01 16:02:46 -0800

    Lab 10 part 1

--- processing/app.py
+++ processing/app.py
@@ -6,21 +6,33 @@ import datetime
 import logging
 import logging.config
 import requests
+import os
 from apscheduler.schedulers.background import BackgroundScheduler
 from flask_cors import CORS, cross_origin
 # from base import Base
 # from item_creation import CreateItem
 # from trade_item import TradeItem
 
+if "TARGET_ENV" in os.environ and os.environ["TARGET_ENV"] == "test":
+    print("In Test Environment")
+    app_conf_file = "/config/app_conf.yml"
+    log_conf_file = "/config/log_conf.yml"
+else:
+    print("In Dev Environment")
+    app_conf_file = "app_conf.yml"
+    log_conf_file = "log_conf.yml"
 
-with open('app_conf.yml', 'r') as f:
+with open(app_conf_file, 'r') as f:
     app_config = yaml.safe_load(f.read())
 
-with open('log_conf.yml', 'r') as f:
+with open(log_conf_file, 'r') as f:
     log_config = yaml.safe_load(f.read())
     logging.config.dictConfig(log_config)
 
 logger = logging.getLogger('basicLogger')
+
+logger.info("App Conf File: %s" % app_conf_file)
+logger.info("Log Conf File: %s" % log_conf_file)
 
 def init_scheduler():
     sched = BackgroundScheduler(daemon=True)
