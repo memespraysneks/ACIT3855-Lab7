62b712c98c8c4fad22f45d49aece56940404d676
memespraysneks <calebsjseeman@gmail.com>
2023-12-08 10:57:27 -0800

    Added log files for the host vm

--- storage/app.py
+++ storage/app.py
@@ -6,6 +6,7 @@ import time
 import yaml
 import datetime
 import logging
+import os
 import logging.config
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -18,18 +19,29 @@ from pykafka.common import OffsetType
 from threading import Thread
 from sqlalchemy import and_
 
-with open('app_conf.yml', 'r') as fs:
-    app_config = yaml.safe_load(fs.read())
+if "TARGET_ENV" in os.environ and os.environ["TARGET_ENV"] == "test":
+    print("In Test Environment")
+    app_conf_file = "/config/app_conf.yml"
+    log_conf_file = "/config/log_conf.yml"
+else:
+    print("In Dev Environment")
+    app_conf_file = "app_conf.yml"
+    log_conf_file = "log_conf.yml"
+with open(app_conf_file, 'r') as f:
+    app_config = yaml.safe_load(f.read())
+    # External Logging Configuration
+with open(log_conf_file, 'r') as f:
+    log_config = yaml.safe_load(f.read())
+    logging.config.dictConfig(log_config)
+logger = logging.getLogger('basicLogger')
+logger.info("App Conf File: %s" % app_conf_file)
+logger.info("Log Conf File: %s" % log_conf_file)
+
 
 DB_ENGINE = create_engine(f'mysql+pymysql://{app_config["datastore"]["user"]}:{app_config["datastore"]["password"]}@{app_config["datastore"]["hostname"]}:{app_config["datastore"]["port"]}/{app_config["datastore"]["db"]}')
 Base.metadata.bind = DB_ENGINE
 DB_SESSION = sessionmaker(bind=DB_ENGINE)
 
-with open('log_conf.yml', 'r') as f:
-    log_config = yaml.safe_load(f.read())
-    logging.config.dictConfig(log_config)
-
-logger = logging.getLogger('basicLogger')
 
 # def item_creation(body):
 #     """ Creates a new item """
